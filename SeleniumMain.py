3b70152483c8613844e46b8b932c4ee09eaa3122
Nguyen Thanh <73066048+thanhprao@users.noreply.github.com>
2023-06-29 17:10:35 +0700

    Update SeleniumMain.py

--- SeleniumMain.py
+++ SeleniumMain.py
@@ -1,28 +1,34 @@
 from selenium import webdriver
-from flask import Flask, request
+from flask import Flask, render_template , request
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='Template')
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-def download_selenium():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sanbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install),options=chrome_options)
+@app.route("/automation",methods = ['Get','POST'])
+def run_automation():
+    if request.method== 'POST':
+        search_key = request.form.get('search_key')
+        title = selenium_code(search_key)
+
+def selenium_code(search_key):
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s)
+    driver.maximize_window()
     driver.get("https://google.com")
+    driver.find_element(By.XPATH,"//input[@name='q']").send_keys(search_key)
+    driver.find_element(By.XPATH,"//input[@name = 'btnK']").submit()
     title = driver.title
-    language = driver.find_element(By.XPATH,"//div[id='SivCob']").text
-    data = {'Title': title,'Language': language}
-    return data
+    
+    return(title)
+   
 
-@app.route('/',methods = ['GET','POST'])
-def home():
-    if(request.method =='GET'):
-        return download_selenium()
+
 
 if __name__ == "__main__":
-    app.run(debug=True,port=3000)
+    app.run(debug=True)
